a04e39c51b6b68486b67c3dc4de4fa2b64a5803c
jhony112 <shipaclick@gmail.com>
2020-07-15 08:25:37 +0100

    cleanup

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -2,86 +2,79 @@ export default {
   data() {
     return {
       handler: null,
-      color: '',
+      color: ""
     };
   },
-  mounted: function () {
-
-  },
+  mounted: function() {},
   methods: {
     alert(title, msg, callback) {
-      this.$ons.notification.alert(msg, { title: title ? title : 'Error' })
-        .then((response) => {
+      this.$ons.notification
+        .alert(msg, { title: title ? title : "Error" })
+        .then(response => {
           if (callback) {
             callback();
           }
           // Handle response.
         });
     },
-    prompt(title, msg,) {
-      return this.$ons.notification.alert(msg, { title: title ? title : 'Error' });
+    prompt(title, msg) {
+      return this.$ons.notification.alert(msg, {
+        title: title ? title : "Error"
+      });
     },
     statusBar(color) {
       if (this.$isLiveApp) {
-
-        console.log('live app so chnage statusbar');
-        if (color === 'default') {
+        console.log("live app so chnage statusbar");
+        if (color === "default") {
           // StatusBar.overlaysWebView(false);
           StatusBar.styleBlackOpaque();
-        }
-        else {
+        } else {
           StatusBar.backgroundColorByHexString(color);
         }
-
-
       }
     },
-    push(page, params = {}, anim = 'slide-ios') {
-      this.$store.commit('navigator/options', {
+    push(page, params = {}, anim = "slide-ios") {
+      this.$store.commit("navigator/options", {
         // Sets animations
         animation: anim,
         // Resets default options
-        callback: () => this.$store.commit('navigator/options', {})
+        callback: () => this.$store.commit("navigator/options", {})
       });
       if (params.statusBar && this.$isLiveApp) {
         this.statusBar(params.statusBar);
       }
-      this.$store.commit('navigator/push', {
+      this.$store.commit("navigator/push", {
         extends: page,
         data() {
           return {
             toolbarInfo: {
-              title: params.key ? params.key : ''
+              title: params.key ? params.key : ""
             }
           };
         }
       });
     },
     back(params = {}) {
-
       if (params.statusBar) {
         this.statusBar(params.statusBar);
       }
       // if (params.statusBar && this.$isLiveApp) {
       //     this.statusBar(params.statusBar)
       // }
-      this.$store.commit('navigator/pop');
-
+      this.$store.commit("navigator/pop");
     },
     moneyFormat(m, dec = false) {
       if (dec) {
         return parseFloat(m)
           .toFixed(2)
-          .replace(/\d(?=(\d{3})+\.)/g, '$&,');
+          .replace(/\d(?=(\d{3})+\.)/g, "$&,");
       }
       return parseFloat(m)
         .toFixed(0)
-        .replace(/\d(?=(\d{3})+\.)/g, '$&,');
-
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
     },
     resetTo(page) {
-      return this.$store.commit('navigator/reset', page);
-
+      return this.$store.commit("navigator/reset", page);
     },
     hideSplashScreen() {
       if (this.$isLiveApp) {
@@ -108,15 +101,13 @@ export default {
     isObject(obj) {
       return obj !== undefined && obj !== null && obj.constructor === Object;
     },
-    toast(msg, type = 'error', direction = 'down', duration = 4000) {
-
+    toast(msg, type = "error", direction = "down", duration = 4000) {
       if (this.isObject(msg)) {
         msg = JSON.stringify(msg)
-          .replace(/]|[[]/g, '')
-          .replace('{', ' ')
-          .replace('}', ' ')
-          .replace(' " ', ' ');
-
+          .replace(/]|[[]/g, "")
+          .replace("{", " ")
+          .replace("}", " ")
+          .replace(' " ', " ");
       }
 
       // window.$('#toast')
@@ -134,20 +125,16 @@ export default {
       //     .append('<style id="toast" type="text/css">.toast{background: #0d8846!important;}</style>');
       // }
 
-
-      let anim = (direction === 'down') ? 'ascend' : 'fall';
+      let anim = direction === "down" ? "ascend" : "fall";
       return this.$ons.notification.toast(msg, {
         timeout: duration,
         modifier: type,
         animation: anim
       });
-
-
-    },
-
+    }
   },
 
   beforeDestroy() {
-    document.removeEventListener('keyup', this.handler);
+    document.removeEventListener("keyup", this.handler);
   }
 };
